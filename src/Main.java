ff645ee1e4472d2590541b436a174bd84d9b1572
pai-puella <cat.dyshkant@yandex.ru>
2023-10-02 12:42:34 +0300

    Added Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
 
+        double[] arr2 = {1.57, 7.654, 9.986};
+
+        String[] arr3 = {"Hello", "World"};
+        System.out.println();
+    }
 }
