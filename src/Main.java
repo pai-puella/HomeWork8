52167355f73e23672721ae84721c14a3b2e4ec1c
pai-puella <cat.dyshkant@yandex.ru>
2023-10-02 12:46:56 +0300

    Added Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1");
+        // Задача 1
         int[] arr1 = new int[3];
         arr1[0] = 1;
         arr1[1] = 2;
@@ -9,6 +11,11 @@ public class Main {
         double[] arr2 = {1.57, 7.654, 9.986};
 
         String[] arr3 = {"Hello", "World"};
+
+        System.out.println("Задача 2");
+        System.out.println(Arrays.toString(arr1).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(arr2).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(arr3).replace("[", "").replace("]", ""));
         System.out.println();
     }
 }
